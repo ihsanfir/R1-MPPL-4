ed81a81206e07ddf15343abe962523fc76f948cd
sabrinadizam21 <sabrinadizam21@gmail.com>
2021-10-27 16:17:40 +0700

    add template and change dashboard, employee FE

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -22,4 +22,28 @@
         </article>
         @endforeach
     </body>
-</html>
+</html> -->
+
+@extends('master')
+@section('title') IFISH | {{ $title }} @endsection
+@section('heading_1') DASHBOARD @endsection
+@section('content')
+    <div class="row">
+    @foreach ($expanse as $expanses)
+        <article>
+            {{ $expanses->amount }}
+            {{ $expanses->date }}
+        </article>
+        @endforeach
+    </div>
+
+    <div class="row">
+        <h2 class="m-2 text-dark"> Income </h2>
+        @foreach ($income as $incomes)
+            <article>
+                {{ $incomes->amount }}
+                {{ $incomes->date }}
+            </article>
+        @endforeach
+    </div>
+@endsection
