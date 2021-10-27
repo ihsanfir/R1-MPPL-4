cdd2d49bf2cca9afebe4ff9f46cfaedf99867d71
Ihsan Firdaus <ihsanfirdaus16@gmail.com>
2021-10-27 20:45:22 +0700

    delete auth

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -66,7 +66,8 @@
           <img src="{{asset('vendors/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">{{ auth()->user()->name }}</a>
+          <!-- <a href="#" class="d-block">{{ auth()->user()->name }}</a> -->
+          <a href="#" class="d-block">Kang IFISH</a>
         </div>
       </div>
 
