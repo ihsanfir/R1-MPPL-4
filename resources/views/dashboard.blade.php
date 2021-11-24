3a4f88a06788f30757e48d1ff0aec85930436485
sabrinadizam21 <sabrinadizam21@gmail.com>
2021-11-25 00:26:31 +0700

    add table recently added product on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,12 +8,6 @@
           <div class="col-sm-6">
             <h1 class="m-0">Dashboard</h1>
           </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard</li>
-            </ol>
-          </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
 </section>
@@ -159,6 +153,53 @@
             <!-- /.card -->
       
           <!-- /.col (RIGHT) -->
+
+          <!-- PRODUCT LIST -->
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Recently Added Products</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body p-0">
+                <ul class="products-list product-list-in-card pl-2 pr-2">
+                  <?php $i = 0; ?>
+                  @foreach($products as $pro)
+                  @if($i < 5)
+                  <li class="item">
+                    <div class="product-img">
+                      <img src="{{asset('vendors/dist/img/default-150x150.png')}}" alt="Product Image" class="img-size-50">
+                    </div>
+                    <div class="product-info">
+                      <a  class="product-title">{{$pro->name}}
+                        <span class="badge badge-success float-right">{{$pro->stock}}</span></a>
+                        @if($i == 0) <span class="right badge badge-danger">New</span> @endif
+                      <span class="product-description">
+                      {{$pro->product_supplier->name}}
+                      </span>
+                    </div>
+                  </li>
+                  <?php $i++; ?>
+                  @endif
+                  @endforeach
+                  <!-- /.item -->
+                </ul>
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer text-center">
+                <a href="{{url('/product')}}" class="uppercase">View All Products</a>
+              </div>
+              <!-- /.card-footer -->
+            </div>
+            <!-- /.card -->
         </div>
         <!-- /.row -->
       </div><!-- /.container-fluid -->
