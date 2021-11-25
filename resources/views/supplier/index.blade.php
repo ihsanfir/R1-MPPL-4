59c81173d369a89f53575039850371e6b4bc3737
irlanfazryana23 <irlan.fazryana23@gmail.com>
2021-11-25 14:07:24 +0700

    rapihin ui

--- resources/views/supplier/index.blade.php
+++ resources/views/supplier/index.blade.php
@@ -8,22 +8,20 @@
 
 @section('content')
 <body>
-<section class="content-header">
-    <div class="container-fluid">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>Suppliers</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
-                        <li class="breadcrumb-item active">Suppliers</li>
-                    </ol>
-                </div>
-            </div>
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Suppliers</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
+              <li class="breadcrumb-item active">Suppliers</li>
+            </ol>
+          </div>
         </div>
-    </div>
+      </div><!-- /.container-fluid -->
 </section>
         <div class="row">
           <div class="col-12">
